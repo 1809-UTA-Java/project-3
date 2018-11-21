2027d4e799c2541cfb28fa03a1dcf6634cb00396
ricardoaranda <ricardomessier@gmail.com>
2018-11-21 09:52:11 -0600

    added missing freeze-account api. Added edited gitignore file to exclude OSX specific files.

---
+++ SQL/Retail/frozen-accounts-ddl.sql
@@ -0,0 +1,14 @@
+--------------------------------------------------------
+--  File created - Monday-November-19-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table FROZENACCOUNTS
+--------------------------------------------------------
+
+  CREATE TABLE "FROZENACCOUNTS"."FROZENACCOUNTS" 
+   (	"ACCOUNTID" VARCHAR2(40 BYTE), 
+	"USERID" VARCHAR2(40 BYTE), 
+	"TIMESUBMITTED" VARCHAR2(40 BYTE), 
+	"AUTHOR" VARCHAR2(40 BYTE), 
+	"ACCOUNTSTATUS" VARCHAR2(40 BYTE)
+   );
