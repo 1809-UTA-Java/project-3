cf68c75ff82d42ead12aa42654ab2c988ab14038
ricardoaranda <ricardomessier@gmail.com>
2018-11-26 15:38:09 -0600

    added styling to splash page

--- MuleSoft/Combined/nd_financial/src/main/resources/web/registration.js
+++ MuleSoft/Combined/nd_financial/src/main/resources/web/registration.js
@@ -1,43 +1,45 @@
 const regForm = () => {
     return (`
-    <form id='regForm'>
-            <div>
-                <label for='name'>Name:<input type='text' name='name' placeholder='enter name' /></label>
+    <form id='regForm' class="pure-form pure-form-aligned">
+        <fieldset>
+            <div class="pure-control-group">
+                <label for='name'>Name:</label><input type='text' name='name' placeholder='enter name' />
             </div>
-            <div>
-                <label for='username'>Username:<input type='text' name='username' placeholder='enter username' required /></label>
+            <div class="pure-control-group">
+                <label for='username'>Username:</label><input type='text' name='username' placeholder='enter username' required />
             </div>
-            <div>
-                <label for='password'>Password:<input type='password' name='password' placeholder='enter password'
-                        required /></label>
+            <div class="pure-control-group">
+                <label for='password'>Password:</label><input type='password' name='password' placeholder='enter password'
+                        required />
             </div>
-            <div>
-                <label for='gender'>Gender:<input type='text' name='gender' placeholder='enter gender' /></label>
+            <div class="pure-control-group">
+                <label for='gender'>Gender:</label><input type='text' name='gender' placeholder='enter gender' />
             </div>
-            <div>
-                <label for='ethnicity'>Ethnicity:<input type='text' name='ethnicity' placeholder='enter ethnicity' /></label>
+            <div class="pure-control-group">
+                <label for='ethnicity'>Ethnicity:</label><input type='text' name='ethnicity' placeholder='enter ethnicity' />
             </div>
-            <div>
-                <label for='dob'>Date of Birth:<input type='date' name='dob' placeholder='enter dob' /></label>
+            <div class="pure-control-group">
+                <label for='dob'>Date of Birth:</label><input type='date' name='dob' placeholder='enter dob' />
             </div>
-            <div>
-                <label for='address'>Address:<input type='text' name='address' placeholder='enter address' /></label>
+            <div class="pure-control-group">
+                <label for='address'>Address:</label><input type='text' name='address' placeholder='enter address' />
             </div>
-            <div>
-                <label for='email'>Email:<input type='email' name='email' placeholder='enter email' required /></label>
+            <div class="pure-control-group">
+                <label for='email'>Email:</label><input type='email' name='email' placeholder='enter email' required />
             </div>
-            <div>
-                <label for='phone'>Phone Number:<input type='tel' name='phone' placeholder='enter phone number' /></label>
+            <div class="pure-control-group">
+                <label for='phone'>Phone Number:</label><input type='tel' name='phone' placeholder='enter phone number' />
             </div>
-            <div>
-                <label for='occupation'>Occupation:<input type='text' name='occupation' placeholder='enter occupation' /></label>
+            <div class="pure-control-group">
+                <label for='occupation'>Occupation:</label><input type='text' name='occupation' placeholder='enter occupation' />
             </div>
-            <div>
-                <label for='salary'>Salary:<input type='number' name='salary' placeholder='enter yearly salary' /></label>
+            <div class="pure-control-group">
+                <label for='salary'>Salary:</label><input type='number' name='salary' placeholder='enter yearly salary' />
             </div>
+            </fieldset>
         </form>
-        <input type='button' value='Clear' id='btnClear' />
-        <input type='button' value='Submit' id='btnSubmit' />
+        <input type='button' value='Clear' id='btnClear' class='pure-button'/>
+        <input type='button' value='Submit' id='btnSubmit' class='pure-button'/>
         <div id='error'></div>
     </div>
     `);
